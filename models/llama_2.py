bd1314c995413f131a6d9f2618c1a9e457f11e46
Tushar Goyal <92171383+thetushargoyal@users.noreply.github.com>
2024-12-05 14:40:22 +0530

    added llama_2 transformer file

---
+++ models/llama_2.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from dataclasses import dataclass
+from typing import Optional
+
+@dataclass
+class ModelArgs:
+    dim: int = 4096
+    n_layers: int = 32
+    n_heads: int = 32
+    n_kv_heads: Optional[int] = None
+    vocab_size: int = -1
+    multiple_of: int = 256
+    ffn_dim_multiplier: Optional[float] = None
+    norm_eps: float = 1e-5
+
+    max_batch_size: int = 32
+    max_seq_len: int = 2048
+
+    device: str = None
+
+class Transformer(nn.Module):
+    
+    def __init__(self, args: ModelArgs) -> None:
+        super().__init__()
+
+        assert args.vocab_size != -1, "Vocab size must be set"
+        self.args = args
+        self.vocab_size = args.vocab_size
+        self.n_layers = args.n_layers
+        self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
+
+        self.layers = nn.ModuleList()
+        for _ in range(args.n_layers):
+            break
