bc4ee53a725aaab2b260a00538d4558e6a558ba4
Tushar Goyal <92171383+thetushargoyal@users.noreply.github.com>
2024-12-08 14:26:09 +0530

    Update train.py

--- models/llama_3/train.py
+++ models/llama_3/train.py
@@ -9,9 +9,7 @@ import torch
 # Using Tiny Shakespeare dataset for character-level tokenizer. Some part of the following character-level tokenizer is referenced from Andrej karpathy's GitHub (https://github.com/karpathy/nanoGPT/blob/master/data/shakespeare_char/prepare.py) which I found is explained very well.
 # Load tiny_shakespeare data file (https://github.com/tamangmilan/llama3/blob/main/tiny_shakespeare.txt)
 
-
 # Define tensor token variable to be used later during model training
-
 
 # Create a dataset by encoding the entire tiny_shakespeare data token_ids list using the tokenizer's encode function that we've built at the input block section
 dataset, vocab, stoi, itos = load_dataset("models/llama_3/dataset/tiny_shakespeare.txt")
